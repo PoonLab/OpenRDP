bc107868d0d14fb8c0d47053ced5085e061571e1
ArtPoon <artpoon@gmail.com>
2023-02-02 15:50:11 -0500

    setup.py now places executable openrdp script in /usr/local/bin

--- setup.py
+++ setup.py
@@ -1,27 +1,29 @@
 # !/usr/bin/env python3
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='OpenRDP',
     version="0.0.1",
     description='Open Source implementation of RDP5',
-    packages=find_packages(include=['openrdp', 'openrdp.*']),
-    # packages=['openrdp'],
+    packages=['openrdp'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent'
     ],
     install_requires=[
-        'scipy>=1.5.0,<1.6.0',
-        'numpy>=1.17.4,<1.20.0',
+        'scipy>=1.5.0',
+        'numpy>=1.17.4',
     ],
     python_requires='>=3.6',
+    scripts=['bin/openrdp'],
+    options={'build_scripts': {'executable': '/usr/bin/env python3'}},
     package_data={'openrdp': [
         'tests/*.fasta',
         'tests/*.fa',
         'tests/*.ini',
         'bin/3Seq/*',
-        'bin/GENECONV/*'
+        'bin/GENECONV/*',
+
     ]},
     zip_safe=False
 )

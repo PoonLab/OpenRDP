ee5b232ee847fdace1d1ced166328dd68cb689fa
Williamzekaiwang <williamzekaiwang@gmail.com>
2024-10-05 23:41:29 -0400

    addition to pattern hunting

--- openrdp/siscan.py
+++ openrdp/siscan.py
@@ -3,12 +3,13 @@ import json
 import numpy as np
 from scipy.ndimage import gaussian_filter1d
 from scipy.signal import find_peaks
+from scipy.stats import norm
 
 from .common import identify_recombinant
 
 
 class Siscan:
-    def __init__(self, align, win_size=100, step_size=20, strip_gaps=True, pvalue_perm_num=1100,
+    def __init__(self, align, win_size=150, step_size=100, strip_gaps=True, pvalue_perm_num=1100,
                  scan_perm_num=100, random_seed=3, max_pvalue=0.05, settings=None, ref_align=None, verbose=False):
         """
         Constructs a Siscan object
@@ -81,7 +82,7 @@ class Siscan:
 
     @staticmethod
     def count_patterns(seq_array):
-        a, b, c, d = [np.array(list(i[0])) for i in seq_array]
+        a, b, c, d = seq_array
 
         pat_counts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
@@ -123,6 +124,8 @@ class Siscan:
         # 1 = 2 = 3 = 4
         pat_counts[14] = np.sum(ab & bc & ad)
 
+        print(pat_counts)
+
         return pat_counts
 
     @staticmethod
@@ -134,6 +137,9 @@ class Siscan:
         """
         sum_pat_counts = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
+        sum_pat_counts[0] = pat_counts[1] + pat_counts[6] + pat_counts[7]  # 2 + 7 + 8
+        sum_pat_counts[1] = pat_counts[2] + pat_counts[5] + pat_counts[8]  # 3 + 6 + 9
+        sum_pat_counts[2] = pat_counts[3] + pat_counts[4] + pat_counts[9]  # 4 + 5 + 10
         sum_pat_counts[3] = pat_counts[1] + pat_counts[7] + pat_counts[10] + pat_counts[11]  # 2 + 8 + 11 + 12
         sum_pat_counts[4] = pat_counts[2] + pat_counts[8] + pat_counts[10] + pat_counts[12]  # 3 + 9 + 11 + 13
         sum_pat_counts[5] = pat_counts[3] + pat_counts[9] + pat_counts[11] + pat_counts[12]  # 4 + 10 + 12 + 13
@@ -143,15 +149,31 @@ class Siscan:
 
         return sum_pat_counts
 
-    def sum_informative(self, pat_counts, sum_pat_counts):
-        '''
-        step 3 where you count the informative sites, done in place
-        
-        return void
-        '''
-        sum_pat_counts[0] = pat_counts[1] + pat_counts[6] + pat_counts[7]  # 2 + 7 + 8
-        sum_pat_counts[1] = pat_counts[2] + pat_counts[5] + pat_counts[8]  # 3 + 6 + 9
-        sum_pat_counts[2] = pat_counts[3] + pat_counts[4] + pat_counts[9]  # 4 + 5 + 10
+    def find_interval(major, minor, ind):
+        """
+        major zlist
+        minor zlist
+        ind of list to start looking
+        """
+        z_cut = norm.ppf(1 - 0.05/len(major)) # zscore corrected to number of windows
+        end = int
+        while minor[ind] > major[ind] and minor[ind] > z_cut:
+            end += 1
+        return int, end
+
+    def find_signal(self, major, minor1, minor2):
+        """
+        major_comb is pattern/sum set of two most related
+        minor_combs is least related
+        """
+        min1_intervals, min2_intervals = [], []
+        z_cut = norm.ppf(1 - 0.05/len(major))
+        for ind, maj, min1, min2 in enumerate(zip(major, minor1, minor2)):
+            if min1 > maj and min1 > z_cut:
+                min1_intervals.append(self.find_interval(maj, min1, ind))
+            if min2 > maj and min2 > z_cut:
+                min2_intervals.append(self.find_interval(maj, min1, ind))
+        return min1_intervals, min2_intervals
 
     def execute(self, triplet):
         """
@@ -162,8 +184,8 @@ class Siscan:
         np.random.seed(self.random_seed)
 
         # Initialize list to map z_values to window positions
-        z_pattern_values = [[0 for i in range(14)] for i in range(triplet.sequences.shape[1])]
-        z_sum_values = [[0 for i in range(9)] for i in range(triplet.sequences.shape[1])] 
+        z_pattern_values = [[None for i in range(14)] for i in range(triplet.sequences.shape[1])]
+        z_sum_values = [[None for i in range(9)] for i in range(triplet.sequences.shape[1])] 
 
 
         # Based on leading edge of the window
@@ -188,25 +210,22 @@ class Siscan:
             seq_array = np.array([a, b, c, d])
             pat_counts = self.count_patterns(seq_array)
 
-            # (2) Sum counts where 2 sequences are identical
+            # (2) Sum counts where 2 sequences are identical 
+            # RDP5 does 2 and three together along with permutations
             sum_pat_counts = self.sum_pattern_counts(pat_counts)
 
-            # (3) Sum counts of each kind of informative site for each window
-            self.sum_informative(pat_counts, sum_pat_counts)
-
-            # (4) Create 4 vertically randomized sequences (steps 1 and 2), repeat for 100 times
+            # (4) Create 4 vertically randomized sequences (steps 1 and 2 + 3 [as per RDP5]), repeat for 100 times
             p_counts = []
             sum_p_counts = []
             for i in range(self.scan_perm_num):
 
                 # Generate 4 vertically randomized sequences (shuffle the values in the columns)
-                a1 = seq_array[:, np.random.permutation(seq_array.shape[1])]
+                a1 = np.apply_along_axis(np.random.permutation, 0, seq_array)
 
                 # Count number of patterns and sum counts
                 curr_p = self.count_patterns(a1)
                 p_counts.append(curr_p)
                 curr_s = self.sum_pattern_counts(curr_p)
-                self.sum_informative(curr_p, curr_s)
                 sum_p_counts.append(curr_s)
 
             # (5) Calculate Z-scores for each pattern and sum of patterns for each window, position should be middle of window            
@@ -217,41 +236,44 @@ class Siscan:
 
             z_pat_counts = [0 for i in pat_counts]
             for num, value in enumerate(pat_counts): # if none it will give error
-                z_pat_counts[num] = float((value - pop_mean_pcounts[num]) / pop_std_pcounts[num])
+                z_pat_counts[num] = (value - pop_mean_pcounts[num]) / pop_std_pcounts[num]
                 
             z_sum_counts = [0 for i in sum_pat_counts]
             for num, value in enumerate(sum_pat_counts):
-                z_sum_counts[num] = float((value - pop_mean_patsum[num]) / pop_std_patsum[num])
+                z_sum_counts[num] = (value - pop_mean_patsum[num]) / pop_std_patsum[num]
 
             # update to total by middle of window
             z_pattern_values[(window + win_end)//2] = z_pat_counts
             z_sum_values[(window + win_end)//2] = z_sum_counts
 
-        with open('pre_guas_counts.json', 'w') as file:
-            json.dump(z_pattern_values, file, indent=4)
+        seq_1_2 = [z_pat_counts[3], z_pat_counts[9], z_sum_counts[2], z_sum_counts[5]] # p2/8, s1/4, 1==2
+        seq_1_3 = [z_pat_counts[4], z_pat_counts[10], z_sum_counts[6], z_sum_counts[7]] # p3/9, s5/6, 1==3
+        seq_2_3 = [z_pat_counts[6], z_pat_counts[11], z_sum_counts[4], z_sum_counts[8]] # p5/10, s3/7, 2==3
 
-        with open('pre_gaus_sums.json', 'w') as file:
-            json.dump(z_sum_values, file, indent=4)
+        # find major combination
+        major, minor = None, None
+        #
+        if major == 1: # seq1/2 major 
+            self.find_signal(seq_1_2, seq_1_3, seq_2_3)
+        if major == 2: # seq2/3 major
+            self.find_signal(seq_2_3, seq_1_2, seq_1_3)
+        if major == 3: # seq1/3 major
+            self.find_signal(seq_1_3, seq_1_2, seq_2_3)
+            
+        # peaks = find_peaks(sum_pat_zscore, distance=self.win_size)
+        # for k, peak in enumerate(peaks[0]):
+        #     search_win_size = 1
+        #     while peak - search_win_size > 0 \
+        #             and peak + search_win_size < len(sum_pat_zscore) - 1 \
+        #             and sum_pat_zscore[peak + search_win_size] > 0.3 * sum_pat_zscore[peak] \
+        #             and sum_pat_zscore[peak - search_win_size] > 0.3 * sum_pat_zscore[peak]:
+        #         search_win_size += 1
 
-        exit()
+        #     if sum_pat_zscore[peak + search_win_size] > sum_pat_zscore[peak - search_win_size]:
+        #         aln_pos = (int(peak), int(peak + search_win_size + self.win_size))
+        #     else:
+        #         aln_pos = (int(peak - search_win_size), int(peak + self.win_size))
 
-        # Smooth z-values
-        sum_pat_zscore = gaussian_filter1d(sum_pat_zscore, 1.5)
-
-        peaks = find_peaks(sum_pat_zscore, distance=self.win_size)
-        for k, peak in enumerate(peaks[0]):
-            search_win_size = 1
-            while peak - search_win_size > 0 \
-                    and peak + search_win_size < len(sum_pat_zscore) - 1 \
-                    and sum_pat_zscore[peak + search_win_size] > 0.3 * sum_pat_zscore[peak] \
-                    and sum_pat_zscore[peak - search_win_size] > 0.3 * sum_pat_zscore[peak]:
-                search_win_size += 1
-
-            if sum_pat_zscore[peak + search_win_size] > sum_pat_zscore[peak - search_win_size]:
-                aln_pos = (int(peak), int(peak + search_win_size + self.win_size))
-            else:
-                aln_pos = (int(peak - search_win_size), int(peak + self.win_size))
-
-            rec_name, parents = identify_recombinant(triplet, aln_pos)
-            if (rec_name, parents, *aln_pos, abs(sum_pat_zscore[peak])) not in self.raw_results:
-                self.raw_results.append((rec_name, parents, *aln_pos, abs(sum_pat_zscore[peak])))
+        #     rec_name, parents = identify_recombinant(triplet, aln_pos)
+        #     if (rec_name, parents, *aln_pos, abs(sum_pat_zscore[peak])) not in self.raw_results:
+        #         self.raw_results.append((rec_name, parents, *aln_pos, abs(sum_pat_zscore[peak])))
